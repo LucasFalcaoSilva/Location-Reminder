67f2053cff722b77c65f993ab84b8f07c4e93f4a
Lucas Miranda <lucaslibra@gmail.com>
2022-01-05 20:40:53 -0300

    clean TODO authentication

--- app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -51,12 +51,6 @@ class AuthenticationActivity : AppCompatActivity() {
 			})
 
 		}
-
-//          TODO: If the user was authenticated, send him to RemindersActivity
-
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-		//https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
-
 	}
 
 	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
